739195203423a950612b9d126558d735188d2649
joaovicsaa <joao.vsaa90@gmail.com>
2024-01-16 11:00:43 -0300

    feat: Organização de Props e Componentes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,34 @@
-import './App.css';
-import { Component } from 'react';
+/** @format */
 
+import './App.css'
+import { Component } from 'react'
+
+import { loadPosts } from './utils/load-posts'
+import { Posts } from './components/Posts'
 
 class App extends Component {
   state = {
-    posts: []
-  };
+    posts: [],
+  }
 
-  componentDidMount () {
-    setTimeout(() => {    this.setState({
-        posts: [
-          {
-            id: 1,
-            title: 'O título 1',
-            body: 'O corpo 1'
-          },
-          {
-            id: 2,
-            title: 'O título 2',
-            body: 'O corpo 2'
-          },
-          {
-            id: 3,
-            title: 'O título 3',
-            body: 'O corpo 3'
-          },
-        ]
-      })
-    }, 5000)
+  async componentDidMount() {
+    await this.loadPosts()
+  }
+
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts()
+
+    this.setState({ posts: postsAndPhotos })
   }
 
   render() {
-    const { posts } = this.state;
- 
+    const { posts } = this.state
+
     return (
-      <div className="App">
-        { posts.map(post => (
-          <div key={post.id}>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-        )
-        ) }
-      </div>
-    );
+      <section className="container">
+        <Posts posts={posts} />
+      </section>
+    )
   }
 }
-export default App;
+export default App
