2c55ffb57175c7762bee8e859071aa26ff85e70b
joaovicsaa <joao.vsaa90@gmail.com>
2024-01-16 14:27:46 -0300

    feat: Adiciona Search bar de posts

---
+++ src/components/TextInput/index.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import './styles.css'
+
+export const TextInput = ({ searchValue, handleChange }) => {
+  return (
+    <input
+      className="text-input"
+      type="search"
+      onChange={handleChange}
+      value={searchValue}
+      placeholder="Type your search"
+    />
+  )
+}
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -6,13 +6,15 @@ import { Component } from 'react'
 import { loadPosts } from '../../utils/load-posts'
 import { Posts } from '../../components/Posts'
 import { Button } from '../../components/Button'
+import { TextInput } from '../../components/TextInput'
 
 class Home extends Component {
   state = {
     posts: [],
     allPosts: [],
     page: 0,
-    postsPerPage: 10,
+    postsPerPage: 2,
+    searchValue: '',
   }
 
   async componentDidMount() {
@@ -39,19 +41,52 @@ class Home extends Component {
     this.setState({ posts, page: nextPage })
   }
 
+  handleChange = (e) => {
+    const { value } = e.target
+    this.setState({ searchValue: value })
+  }
+
   render() {
-    const { posts, postsPerPage, page, allPosts } = this.state
+    const { posts, postsPerPage, page, allPosts, searchValue } = this.state
     const noMorePosts = page + postsPerPage >= allPosts.length
+
+    // operação ternária
+    const filteredPosts = !!searchValue
+      ? allPosts.filter((post) => {
+          return post.title.toLowerCase().includes(searchValue.toLowerCase())
+        })
+      : posts
 
     return (
       <section className="container">
-        <Posts posts={posts} />
+        {/* 
+          curto-circuito 
+
+          !!searchValue transforma o return para boolean, no caso, se 
+          for uma string vazia, return False.
+        */}
+        <div className="search-container">
+          {!!searchValue && (
+            <>
+              <h1>Search Value: {searchValue} </h1>
+            </>
+          )}
+
+          <TextInput searchValue={searchValue} handleChange={this.handleChange} />
+        </div>
+
+        {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+        {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
+
         <div className="button-container">
-          <Button
-            text="Load More Posts"
-            onClick={this.loadMorePosts}
-            disabled={noMorePosts}
-          />
+          {/* simples negação de searchValue */}
+          {!searchValue && (
+            <Button
+              text="Load More Posts"
+              onClick={this.loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
         </div>
       </section>
     )
